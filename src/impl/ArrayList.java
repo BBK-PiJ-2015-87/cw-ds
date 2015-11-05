9882bf76e58d042b4a6a33f8dd37a1ea5038c78f
Vladimirs Ivanovs <londonbim@gmail.com>
2015-11-05 15:51:31 +0000

    Implementation of methods

--- src/impl/ArrayList.java
+++ src/impl/ArrayList.java
@@ -1,5 +1,6 @@
 package impl;
 
+import interfaces.ErrorMessage;
 import interfaces.List;
 import interfaces.ReturnObject;
 
@@ -9,13 +10,13 @@ import interfaces.ReturnObject;
 public class ArrayList implements List {
 
     /**
-     * The array where all elements are stored.
+     * The array where all elements are stored. Default size of this array is 20.
      */
-    private ReturnObject[] elements;
+    private Object[] elements;
 
 
     public ArrayList(int size) {
-        elements = new ReturnObject[size];
+        elements = new Object[size];
     }
 
     public ArrayList() {
@@ -34,7 +35,11 @@ public class ArrayList implements List {
 
     @Override
     public ReturnObject get(int index) {
-        return elements[index];
+        if (!isIndexValid(index)) {
+            return returnOutOfBoundsError();
+        } else {
+            return new ReturnObjectImpl(ErrorMessage.NO_ERROR, elements[index]);
+        }
     }
 
     @Override
@@ -44,11 +49,59 @@ public class ArrayList implements List {
 
     @Override
     public ReturnObject add(int index, Object item) {
-        return null;
+        if (isIndexValid(index)) {
+            return returnSuccess(item);
+        } else if (item == null) {
+            return returnInvalidArgumentError();
+        } else {
+            elements[index] = item;
+            return returnSuccess(item);
+        }
     }
 
     @Override
     public ReturnObject add(Object item) {
         return null;
     }
+
+    private boolean isIndexValid(int index) {
+        if (index < 0 || index >= elements.length) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private ReturnObject returnOutOfBoundsError() {
+        return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS, null);
+    }
+
+    private ReturnObject returnEmptyStructureError() {
+        return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE, null);
+    }
+
+    private ReturnObject returnInvalidArgumentError() {
+        return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT, null);
+    }
+
+    private ReturnObject returnSuccess(Object object) {
+        return new ReturnObjectImpl(ErrorMessage.NO_ERROR, object);
+    }
+
+    private void increaseSize() {
+        int size = elements.length;
+        if (elements[size - 1] != null) {
+            Object[] tmp = elements;
+            elements = new Object[size * 2];
+            copyArrayElements(tmp, elements);
+        }
+    }
+
+    private void copyArrayElements(Object[] src, Object[] dest) {
+        int iter = src.length;
+        while (iter > 0) {
+            dest[iter] = src[iter];
+        }
+    }
+
 }
--- src/impl/ReturnObjectImpl.java
+++ src/impl/ReturnObjectImpl.java
@@ -3,12 +3,19 @@ package impl;
 import interfaces.ErrorMessage;
 import interfaces.ReturnObject;
 
+import java.util.Objects;
+
 /**
  * Created by vladimirsivanovs on 05/11/2015.
  */
 public class ReturnObjectImpl implements ReturnObject {
     private ErrorMessage error;
     private Object returnValue;
+
+    public ReturnObjectImpl(ErrorMessage error, Object returnValue){
+        this.error = error;
+        this.returnValue = returnValue;
+    }
 
     @Override
     public boolean hasError() {
