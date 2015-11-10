9dc6d7c3840e0b20bcbfb4f5d0ab689cd856c0bd
Vladimirs Ivanovs <londonbim@gmail.com>
2015-11-10 14:22:18 +0000

    Updated implementation methods

--- src/impl/ImprovedStackImpl.java
+++ src/impl/ImprovedStackImpl.java
@@ -1,12 +1,18 @@
 package impl;
 
 import interfaces.ImprovedStack;
+import interfaces.List;
 import interfaces.ReturnObject;
 
 /**
  * Created by vladimirsivanovs on 05/11/2015.
  */
-public class ImprovedStackImpl implements ImprovedStack {
+public class ImprovedStackImpl extends StackImpl implements ImprovedStack {
+
+    public ImprovedStackImpl(List list) {
+        super(list);
+    }
+
     @Override
     public ImprovedStack reverse() {
         return null;
@@ -15,30 +21,5 @@ public class ImprovedStackImpl implements ImprovedStack {
     @Override
     public void remove(Object object) {
 
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public void push(Object item) {
-
-    }
-
-    @Override
-    public ReturnObject top() {
-        return null;
-    }
-
-    @Override
-    public ReturnObject pop() {
-        return null;
     }
 }
