705656aa352ba0b865f212bcdb6f76c58ab28c23
BBK-PiJ-2015-87 <londonbim@gmail.com>
2015-11-11 00:21:24 +0000

    Updated Improved Stack class

--- src/impl/ImprovedStackImpl.java
+++ src/impl/ImprovedStackImpl.java
@@ -2,20 +2,36 @@ package impl;
 
 import interfaces.ImprovedStack;
 import interfaces.List;
-import interfaces.ReturnObject;
 
 /**
  * Created by vladimirsivanovs on 05/11/2015.
  */
 public class ImprovedStackImpl extends StackImpl implements ImprovedStack {
 
+    /**
+     * Creates a new abstract stack using the provided list as the
+     * underlying data structure.
+     * <p>
+     * Note: This constructor does not check whether the provided list
+     * is null. Programmers must do their own checks. If a null list
+     * is provided, a NullPointerException will be thrown at runtime
+     * as soon as any operation is attempted on the underlying list.
+     *
+     * @param list the list to be used
+     */
     public ImprovedStackImpl(List list) {
         super(list);
     }
 
+
     @Override
     public ImprovedStack reverse() {
-        return null;
+        LinkedList newList = new LinkedList();
+        for (int i = internalList.size() - 1; i >= 0; i--) {
+            newList.add(internalList.get(i));
+        }
+        ImprovedStack reversed = new ImprovedStackImpl(newList);
+        return reversed;
     }
 
     @Override
